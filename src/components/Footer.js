6a626aae217987e6eb487d104321545eacefb8e9
Vad-Dem <vadikdemidenko@i.ua>
2024-05-03 12:34:28 +0300

    build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ export const Footer = () => {
               <a href="https://www.linkedin.com/in/вадим-демиденко-355a94236/"><img src={navIcon1} alt="Icon" /></a>
               <a href="https://www.instagram.com/vad_demidenko/"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
